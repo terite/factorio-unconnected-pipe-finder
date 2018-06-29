dd8718fc245cba8e42726392dede936a702b6a8e
David Stensland <me@terite.com>
2018-06-29 02:26:16 -0700

    rename to make easier to package

--- data.lua
+++ data.lua
@@ -2,7 +2,7 @@ arrow = util.table.deepcopy(data.raw["arrow"]["orange-arrow-with-circle"])
 arrow.name = "orphan-arrow"
 arrow.circle_picture =
 {
-  filename = "__Unconnected Pipe Finder__/graphics/large-orange-circle.png",
+  filename = "__factorio-unconnected-pipe-finder__/graphics/large-orange-circle.png",
   priority = "low",
   width = "64",
   height = "64"
